9fd4a752c648960c5a63c8ff9bb412556117ee0f
Mohamedweli <mohamedwelihassan80@gmail.com>
2026-09-20 21:36:31 +0300

    Restructured grade_reporter to use a single loop

--- grade_reporter.py
+++ grade_reporter.py
@@ -1,34 +1,32 @@
-### using for loop for grades
-scores = [72, 45, 90, 61, 38]
-for score in scores:
-    if score >= 80:
-        print("A:", score)
-    elif score >= 70:
-        print("B:", score)
-    elif score >= 50:
-        print("C:", score)
-    else:
-        print("F:", score)
-
+### using for loop for grades 
 
 scores = [72, 45, 90, 61, 38]
 
-### Start Counters at 0
 passed_count = 0
 failed_count = 0
+total_score = 0
 
 for score in scores:
-    if score >= 50:
+    # 1. Determine grade & update pass/fail tally at the same time
+    if score >= 80:
+        print("A:", score)
+        passed_count += 1
+    elif score >= 70:
+        print("B:", score)
+        passed_count += 1
+    elif score >= 50:
+        print("C:", score)
         passed_count += 1
     else:
+        print("F:", score)
         failed_count += 1
+
+    # 2. Add to total in the same loop
+    total_score += score
+
+# 3. Print the final summary once at the end
+average = total_score / len(scores)
 
 print("Passed:", passed_count)
 print("Failed:", failed_count)
-
-
-### adding up scores and printing average
-scores = [72, 45, 90, 61, 38]
-
-average = sum(scores) / len(scores)
 print("Average score:", round(average, 1))
